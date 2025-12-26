800b5f08626da7ea1d8a755cb720ca18272981ad
zhangke <zhangke@tentcoo.com>
2025-12-26 09:00:18 +0800

    feat: add renren modules

---
+++ apps/gulimall-coupon/src/main/java/com/nanfeng/gulimall/coupoon/GulimallCoupoonApplication.java
@@ -0,0 +1,13 @@
+package com.nanfeng.gulimall.coupoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCoupoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCoupoonApplication.class, args);
+    }
+
+}
