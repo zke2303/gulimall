7abc7dd83d31981a87e008fb1d136e8d11368129
nanfeng <nanfenggo@gmail.com>
2026-01-01 21:39:36 +0800

    feat: 启动所有module的nacos服务

--- apps/gulimall-ware/src/main/java/com/nanfeng/gulimall/ware/GulimallWareApplication.java
+++ apps/gulimall-ware/src/main/java/com/nanfeng/gulimall/ware/GulimallWareApplication.java
@@ -1,9 +1,13 @@
 package com.nanfeng.gulimall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.nanfeng.gulimall.ware.dao")
 public class GulimallWareApplication {
 
     public static void main(String[] args) {
