586aebb8f2b18cb530992f3269e4cc1a0b4d1629
nanfeng <nanfenggo@gmail.com>
2026-01-01 21:23:56 +0800

    feat: 添加nacos服务

--- apps/gulimall-coupon/src/main/java/com/nanfeng/gulimall/coupon/GulimallCouponApplication.java
+++ apps/gulimall-coupon/src/main/java/com/nanfeng/gulimall/coupon/GulimallCouponApplication.java
@@ -3,8 +3,10 @@ package com.nanfeng.gulimall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.nanfeng.gulimall.coupon.dao")
 public class GulimallCouponApplication {
     public static void main(String[] args) {
