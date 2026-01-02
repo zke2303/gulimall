a1304612190a8575097627e60bd60a01bf96ff44
nanfeng <nanfenggo@gmail.com>
2026-01-02 12:06:55 +0800

    feat(gatewary): 新增gatewary module

---
+++ apps/gulimall-gatewary/src/main/java/com/nanfeng/gulimall/gatewary/GulimallGatewaryApplication.java
@@ -0,0 +1,15 @@
+package com.nanfeng.gulimall.gatewary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GulimallGatewaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewaryApplication.class, args);
+    }
+
+}
---
+++ apps/gulimall-gatewary/src/test/java/com/nanfeng/gulimall/gatewary/GulimallGatewaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanfeng.gulimall.gatewary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallGatewaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
